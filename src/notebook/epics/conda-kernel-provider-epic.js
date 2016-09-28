366e5b267db698a073455b7d59905f70fce7e104
Kyle Kelley <rgbkrk@gmail.com>
2016-09-28 13:06:24 -0700

    Create kernelspecs for each conda environment.

--- src/notebook/epics/conda-kernel-provider-epic.js
+++ src/notebook/epics/conda-kernel-provider-epic.js
@@ -19,8 +19,44 @@ export function condaEnvsObservable(condaInfo$) {
   });
 }
 
+export function condaKernelSpecsObservable(condaPrefix$) {
+  return condaPrefix$;
+}
+
+export function createKernelSpecsFromEnvs(envs) {
+  const displayPrefix = 'Python'; // Or R
+  const languageKey = 'py'; // or r
+
+  // TODO: Handle Windows & Conda
+  const languageExe = 'bin/python';
+  const jupyterBin = 'bin/jupyter';
+
+  const langEnvs = {};
+
+  // Check for existence
+  for (const env of envs) {
+    const base = env.prefix;
+    const exePath = path.join(base, languageExe);
+    // TODO: if exists(path.join(base, jupyter)) and exists(exePath)
+    //       --> will need to write the observable logic for that...
+    // TODO: Alternatively: `bin/Python -m ipykernel --version`
+    const envName = env.name;
+    const name = `conda-env-${envName}-${languageKey}`;
+    langEnvs[name] = {
+      display_name: `${displayPrefix} [conda env:${envName}]`,
+      // TODO: Support default R kernel
+      argv: [exePath, '-m', 'ipykernel', '-f', '{connection_file}'],
+      language: 'python',
+      // TODO: Provide resource_dir
+    };
+  }
+  return langEnvs;
+}
+
+
 export function condaKernelsEpic(action$, store) {
   return action$.ofType('GET_CONDA_KERNELS')
     .switchMap(action => condaEnvsObservable(condaInfoObservable()))
+    .map(createKernelSpecsFromEnvs)
     .map(condaInfo => ({ type: 'CONDA_ENVS', payload: condaInfo }));
 }
